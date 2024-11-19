269168aff83510e412dfc7b7d994b1bebb328271
devHksingh <hksingh.dev@gmail.com>
2024-11-19 21:52:02 +0530

    add script

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5500
 
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.status(201).send('hello docker')
+    res.status(201).send('hello nodejs and docker')
 })
 
 app.listen(port,()=>{
